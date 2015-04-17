791f7a063e3c2333b105f4ce3d73b7dbb68364ce
dhawking <gaeta.d@gmail.com>
2015-04-17 14:03:13 -0500

    stable version with tests

--- e2e/main/main.po.js
+++ e2e/main/main.po.js
@@ -6,9 +6,21 @@
 'use strict';
 
 var MainPage = function() {
-  this.heroEl = element(by.css('.hero-unit'));
-  this.h1El = this.heroEl.element(by.css('h1'));
-  this.imgEl = this.heroEl.element(by.css('img'));
+	//Test content of Intro sections 
+  this.introEl = element(by.css('.intro'));
+  this.introBodyEl = this.introEl.element(by.css('.intro-body'));
+  this.h1El = this.introBodyEl.element(by.css('h1'));
+  this.introTextEl = this.introBodyEl.element(by.css('.intro-text'));
+
+  //Test content of About section 
+  this.aboutEl = element(by.id('about'));
+  this.aboutH2El = this.aboutEl.element(by.css('h2'));
+  this.aboutFirstLineEl = this.aboutEl.element(by.id('about-first-line'));
+
+  //Test content of Contact section
+  this.contactEl = element(by.id('contact'));
+  this.contactH2El = this.contactEl.element(by.css('h2'));
+  this.contactFirstLineEl = this.contactEl.element(by.id('contact-first-line'))
 };
 
 module.exports = new MainPage();
--- e2e/main/main.spec.js
+++ e2e/main/main.spec.js
@@ -8,9 +8,20 @@ describe('Main View', function() {
     page = require('./main.po');
   });
 
-  it('should include jumbotron with correct data', function() {
-    expect(page.h1El.getText()).toBe('\'Allo, \'Allo!');
-    expect(page.imgEl.getAttribute('src')).toMatch(/assets\/images\/yeoman.png$/);
-    expect(page.imgEl.getAttribute('alt')).toBe('I\'m Yeoman');
+  it('should include intro with correct data', function() {
+    expect(page.h1El.getText()).toBe('INFLUENCE');
+    expect(page.introTextEl.getText()).toBe('A peer-to-peer platform for buying and selling social media influence.')
+    //expect(page.imgEl.getAttribute('src')).toMatch(/assets\/images\/yeoman.png$/);
+    //expect(page.imgEl.getAttribute('alt')).toBe('I\'m Yeoman');
+  });
+
+  it('should include about with correct data', function() {
+    expect(page.aboutH2El.getText()).toBe('ABOUT INFLUENCE');
+    expect(page.aboutFirstLineEl.getText()).toBe('It\'s no secret, if someone popular on social media raved about your new product/clothing line/blog, your business could sky rocket.');
+  });
+
+  it('should include contact with correct data', function() {
+    expect(page.contactH2El.getText()).toBe('CONTACT INFLUENCE');
+    expect(page.contactFirstLineEl.getText()).toBe('Feel free to email us to provide some feedback, give us suggestions, or to just say hello!');
   });
 });
