49d09b92c7bfae9cbc8632c657c71c287033ffea
dhawking <gaeta.d@gmail.com>
2015-04-16 19:43:07 -0500

    commiting service creation

---
+++ client/app/twitterService/twitterService.service.js
@@ -0,0 +1,6 @@
+'use strict';
+
+angular.module('buySellInfluenceApp')
+  .service('twitterService', function () {
+    // AngularJS will instantiate a singleton by calling "new" on this function
+  });
